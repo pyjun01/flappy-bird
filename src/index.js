3287925fcb044144749b6048680118a72b520a0a
pyjun01 <pyjun02@gmail.com>
2022-01-19 23:25:53 +0900

    feat: pipe movement

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ canvas.width = width;
 canvas.height = height;
 
 const v = {
-  x: 184,
+  x: width / 2 - 68,
   y: height / 2,
   w: 17 * 4,
   h: 13 * 4,
 };
 let gravity = 0;
 const velocity = 0.5;
+
+const pipes = [];
 
 let reqId = null;
 const stop = () => {
@@ -27,6 +29,12 @@ const render = () => {
 
   ctx.fillStyle = '#000';
   ctx.fillRect(v.x, v.y, v.w, v.h);
+
+  ctx.fillStyle = '#af0';
+  pipes.forEach(({ x, y, w, h }) => {
+    ctx.fillRect(x - w / 2, 0, w, y - h / 2);
+    ctx.fillRect(x - w / 2, y + h / 2, w, height - (y + h / 2));
+  });
 };
 
 const FPS = 1000 / 60;
@@ -51,11 +59,33 @@ canvas.onclick = () => {
   if (reqId === null) {
     reqId = requestAnimationFrame(animate);
 
+    pipes.push({
+      x: width,
+      y: height / 2,
+      w: 26 * 4,
+      h: 65 * 4,
+    });
+
     setInterval(() => {
       v.y += gravity;
 
       if (gravity <= 6) {
         gravity += velocity;
+      }
+
+      pipes.forEach((d) => {
+        d.x = d.x - 1.2;
+      });
+
+      if (pipes[0].x - pipes[0].w / 2 <= v.x && pipes.length === 1) {
+        pipes.push({
+          x: width,
+          y: height / 10 + 130 + Math.floor(Math.random() * 6) * (height / 10),
+          w: 26 * 4,
+          h: 65 * 4,
+        });
+      } else if (pipes[0].x + pipes[0].w / 2 <= 0) {
+        pipes.shift();
       }
     }, 1000 / 100);
   }
